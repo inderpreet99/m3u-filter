8fd31ea0070aa1a7032649ff7bd9686bce5866a8
Inderpreet Singh <inderpreet99@gmail.com>
2017-10-08 23:33:40 -0400

    Indentation

--- index.js
+++ index.js
@@ -7,28 +7,28 @@ exports.handler = function (event, context) {
         filter = event.queryStringParameters.hasOwnProperty('filter') ? true : false;
 
     http.get(url, function (res) {
-    console.log('Success, with: ' + res.statusCode);
+        console.log('Success, with: ' + res.statusCode);
 
 
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', (chunk) => { rawData += chunk; });
-    res.on('end', () => {
-        try {
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            try {
 
-            const res2 = {
-                "statusCode": 200,
-                "headers": { 'Content-Type': 'text/html' },
-                "body": rawData
-            };
+                const res2 = {
+                    "statusCode": 200,
+                    "headers": { 'Content-Type': 'text/html' },
+                    "body": rawData
+                };
 
 
-            context.succeed(res2);
-            context.done(null);
-        } catch (e) {
-            console.error(e.message);
-        }
-    });
+                context.succeed(res2);
+                context.done(null);
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
 
     }).on('error', function (err) {
         console.log('Error, with: ' + err.message);
